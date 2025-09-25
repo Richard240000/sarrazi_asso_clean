2d408aa3675a4b32aba38a8d07335c47bdefda54
carolelucas <clucas@pageup.fr>
2025-09-25 09:57:33 +0200

    fix date to string

--- lib/pages/Actuel/information.dart
+++ lib/pages/Actuel/information.dart
@@ -32,68 +32,43 @@ class _InformationPageState extends State<InformationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Infos pratiques'),
-        backgroundColor: Colors.teal,
-      ),
-      body: actualites.isEmpty
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              padding: const EdgeInsets.all(12.0),
-              itemCount: actualites.length,
-              itemBuilder: (context, index) {
-                final actualite = actualites[index];
-                return Card(
-                  elevation: 4,
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Html(
-                          data: actualite.texte,
-                          onLinkTap: (url, attributes, element) {
-                            if (url != null) {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => WebViewPage(url: url),
-                                ),
-                              );
-                            }
-                          },
-                          style: {
-                            "body": Style(
-                              fontSize: FontSize(16),
-                              color: Colors.black87,
-                            ),
-                            "a": Style(
-                              color: Colors.teal,
-                              textDecoration: TextDecoration.underline,
-                            ),
-                          },
-                        ),
-                        Align(
-                          alignment: Alignment.bottomRight,
-                          child: Text(
-                            actualite.dateAjout,
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontStyle: FontStyle.italic,
-                              color: Colors.grey[600],
-                            ),
+      appBar: AppBar(title: Text('Infos pratiques'), backgroundColor: Colors.teal),
+      body:
+          actualites.isEmpty
+              ? Center(child: CircularProgressIndicator())
+              : ListView.builder(
+                padding: const EdgeInsets.all(12.0),
+                itemCount: actualites.length,
+                itemBuilder: (context, index) {
+                  final actualite = actualites[index];
+                  return Card(
+                    elevation: 4,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Html(
+                            data: actualite.texte,
+                            onLinkTap: (url, attributes, element) {
+                              if (url != null) {
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => WebViewPage(url: url)));
+                              }
+                            },
+                            style: {"body": Style(fontSize: FontSize(16), color: Colors.black87), "a": Style(color: Colors.teal, textDecoration: TextDecoration.underline)},
                           ),
-                        ),
-                      ],
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: Text(DateFormat("dd/MM/yyyy").format(actualite.dateAjout), style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic, color: Colors.grey[600])),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              },
-            ),
+                  );
+                },
+              ),
     );
   }
 }
